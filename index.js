bffc0b2f099c20baa54ae732eca224734d50ef3b
Adebayo Tola <tpharell22@gmail.com>
2022-06-16 20:10:39 +0100

    chore : destroy container

--- index.js
+++ index.js
@@ -4,19 +4,27 @@ const hosts =
   "https://account-dev.wafi.cash,https://connect-dev.wafi.cash,https://sandbox-checkout.wafi.cash,https://sandbox-account.wafi.cash,http://localhost:3000,http://localhost:3001".split(
     ","
   );
+
 const WafiCheckout = {
   launch: (url, options) => {
     const { selector, success, cancel, error } = options;
-    document.getElementById(selector).setAttribute("src", url);
+    const el = document.getElementById(selector);
+    el.setAttribute("src", url);
     window.addEventListener(
       "message",
       function (messageEvent) {
         if (!hosts.includes(messageEvent.origin)) return;
         const { event, data } = messageEvent.data;
 
-        if (event === "success" && success) success(data);
+        if (event === "success" && success) {
+          success(data);
+          el.parentElement.removeChild(el);
+        }
         if (event === "error" && error) error(data);
-        if (event === "cancel" && cancel) cancel(data);
+        if (event === "cancel" && cancel) {
+          cancel(data);
+          el.parentElement.removeChild(el);
+        }
       },
       false
     );

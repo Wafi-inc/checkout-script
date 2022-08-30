60492a5984af337282509b640dba35659df19cba
BunmiAdeleke123 <beninn57@gmail.com>
2022-08-30 08:00:33 +0100

    chore: urgent popup fix

--- checkout.js
+++ checkout.js
@@ -13,7 +13,7 @@
       let popUp;
       let height = 500;
       let width = 700;
-      var left = ( screen.width - width ) / 2;
+      var left = (( screen.width - width ) / 2) + 200;
       var top = ( screen.height - height ) / 2;
 
       let btn = document.createElement("button");

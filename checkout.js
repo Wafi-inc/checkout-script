dac764caed0f8998a1028cb009cecba59b3009f8
BunmiAdeleke123 <beninn57@gmail.com>
2022-09-01 16:15:58 +0100

    chore: urgent fix

--- checkout.js
+++ checkout.js
@@ -10,27 +10,31 @@
   const WafiCheckout = {
     launch: (url, options) => {
       const { success, cancel, error } = options;
-      let popUp;
-      let height = 500;
-      let width = 700;
-      var left = (( screen.width - width ) / 2);
-      var top = ( (screen.height - height ) / 2);
+      // let popUp;
+      // let height = 500;
+      // let width = 700;
+      // var left = (( screen.width - width ) / 2);
+      // var top = ( (screen.height - height ) / 2);
 
-      let btn = document.createElement("button");
-      btn.addEventListener('click',()=>{
-        popUp = window.open(
-          url,
-          "popUpWindow",
-          'resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,width=' + width + ', height=' + height + ', top='+ top + ', left=' + left +',status=yes'
-        );
+      // let btn = document.createElement("button");
+      // btn.addEventListener('click',()=>{
+      //   popUp = window.open(
+      //     url,
+      //     "popUpWindow",
+      //     'resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,width=' + width + ', height=' + height + ', top='+ top + ', left=' + left +',status=yes'
+      //   );
 
-        if (!popUp || popUp.closed || typeof popUp.closed == "undefined") {
-          //...
-          window.location.replace(url)
-        }
-      })
-      btn.click()
+      //   if (!popUp || popUp.closed || typeof popUp.closed == "undefined") {
+      //     //...
+      //     sessionStorage.setItem("wafi-merchant", window.location.href);
+      //     window.location.replace(url)
+      //   }
+      // })
+      // btn.click()
 
+
+      window.location.replace(url)
+      
       window.addEventListener(
         "message",
         function (messageEvent) {
